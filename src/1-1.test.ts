bdeac50c584763603eb40ba2c808908ef52a7ba2
Chris Booty <chrisalan313@gmail.com>
2023-12-07 21:46:14 -0500

    Add last functions for day 1-1

---
+++ src/1-1.test.ts
@@ -0,0 +1,58 @@
+import { afterAll, beforeAll, describe, expect, it, spyOn } from 'bun:test';
+import { combineDigits, extractFirstAndLastDigits, fileTotaller } from '../src/1-1';
+
+describe('integration', () => {
+	it('solves day of advent 1-1', async () => {
+		const file =  Bun.file('./inputs/day-1.txt');
+		console.log(file);
+		const expectedTotal = 1;
+		const actualTotal = await fileTotaller(file);
+
+		expect(actualTotal).toBe(expectedTotal);
+	});
+});
+
+describe('fileTotaller', () => {
+	it('takes a  file and returns the sum of the first and last digits', async () => {
+		const testFile = Bun.file('testFile.txt');
+		await Bun.write(
+			testFile,
+			`rhqrpdxsqhgxzknr2foursnrcfthree
+2bmckl
+four95qvkvveight5
+2tqbxgrrpmxqfglsqjkqthree6nhjvbxpflhr1eightwohr`,
+		);
+		const expectedTotal = 22 + 22 + 95 + 21;
+		const actualTotal = await fileTotaller(testFile);
+
+		expect(actualTotal).toBe(expectedTotal);
+	});
+});
+
+describe('combineDigits', () => {
+	it('concatonates two digits into a single two dgit number', () => {
+		expect(combineDigits(['1', '1'])).toBe(11);
+		expect(combineDigits(['9', '1'])).toBe(91);
+	});
+});
+
+describe('extractFirstAndLastDigits', () => {
+	it('gets the first and last digits in the string', () => {
+		expect(extractFirstAndLastDigits('211a1')).toEqual(['2', '1']);
+		expect(extractFirstAndLastDigits('12')).toEqual(['1', '2']);
+		expect(extractFirstAndLastDigits('7eight7fmqpzrjlcctjvhrdcjgm2mgkqrbdcmzlzngbkqlj1')).toEqual([
+			'7',
+			'1',
+		]);
+	});
+
+	it('returns single digit as first and last digits when only one digit in string', () => {
+		expect(extractFirstAndLastDigits('1')).toEqual(['1', '1']);
+		expect(extractFirstAndLastDigits('a2')).toEqual(['2', '2']);
+	});
+
+	it('throws error if no digit is in string', () => {
+		const str = 'hello';
+		expect(() => extractFirstAndLastDigits(str)).toThrow(Error);
+	});
+});
--- src/1-1.ts
+++ src/1-1.ts
@@ -1,17 +1,32 @@
 // 1. Take the first and last number of the string and concatonate them
 // 2. Sum all the numbers
 
+import { BunFile } from 'bun';
+
+// Solve problem
+
+// Library
+
+export const fileTotaller = async (file: BunFile): Promise<number> => {
+	const text = await file.text();
+	const lines = text.split('\n');
+	const numbers = lines.map<number>((line) => combineDigits(extractFirstAndLastDigits(line)));
+
+	return numbers.reduce((acc, cur) => {
+		return acc + cur;
+	}, 0);
+};
+
 export const combineDigits = (digits: [string, string]): number => {
 	return +digits.join('');
 };
 
-export const extractFirstAndLastDigits = (str: string): string[] | null => {
+export const extractFirstAndLastDigits = (str: string): [string, string] => {
 	// Regex to capture all digits in string
 	const rgx: RegExp = /[0-9]/g;
 	const digits = [...str.matchAll(rgx)].flat();
 	if (digits === undefined || digits.length === 0) {
-		console.error(`No digits in str. Value of str is ${str}`);
-		return null;
+		throw new Error(`No digits in str. Value of str is ${str}`);
 	}
 	const firstDigit = digits[0];
 	const lastDigit = digits.length === 1 ? digits[0] : digits[digits.length - 1];
