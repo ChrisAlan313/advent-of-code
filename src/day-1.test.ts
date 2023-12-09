7acb363fcaa41ab8f0b8a6deadaa09901b0fbac2
Chris Booty <chrisalan313@gmail.com>
2023-12-08 21:06:47 -0500

    Add solution to day 1, part 2

--- src/day-1.test.ts
+++ src/day-1.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'bun:test';
-import { combineDigits, extractFirstAndLastDigits, fileTotaller } from './day-1';
+import { combineDigits, convertWordToNum, extractFirstAndLastDigits, fileTotaller } from './day-1';
 
 describe('integration', () => {
 	it('solves day of advent 1-1', async () => {
@@ -9,22 +9,32 @@ describe('integration', () => {
 
 		expect(actualTotal).toBe(expectedTotal);
 	});
+
+	it('solves day of advent 1-2', async () => {
+		const file = Bun.file(`${import.meta.dir}/inputs/day-1.txt`);
+		const expectedTotal = 54578;
+		const actualTotal = await fileTotaller(file, { readWords: true });
+
+		expect(actualTotal).toBe(expectedTotal);
+	});
 });
 
 describe('fileTotaller', () => {
-	it('takes a  file and returns the sum of the first and last digits', async () => {
-		const testFile = Bun.file('testFile.txt');
-		await Bun.write(
-			testFile,
-			`rhqrpdxsqhgxzknr2foursnrcfthree
-2bmckl
-four95qvkvveight5
-2tqbxgrrpmxqfglsqjkqthree6nhjvbxpflhr1eightwohr`,
-		);
-		const expectedTotal = 22 + 22 + 95 + 21;
-		const actualTotal = await fileTotaller(testFile);
+	describe('without readWords option', () => {
+		it('takes a  file and returns the sum of the first and last digits', async () => {
+			const testFile = Bun.file('testFile.txt');
+			await Bun.write(
+				testFile,
+				`rhqrpdxsqhgxzknr2foursnrcfthree
+	2bmckl
+	four95qvkvveight5
+	2tqbxgrrpmxqfglsqjkqthree6nhjvbxpflhr1eightwohr`,
+			);
+			const expectedTotal = 22 + 22 + 95 + 21;
+			const actualTotal = await fileTotaller(testFile);
 
-		expect(actualTotal).toBe(expectedTotal);
+			expect(actualTotal).toBe(expectedTotal);
+		});
 	});
 });
 
@@ -36,22 +46,51 @@ describe('combineDigits', () => {
 });
 
 describe('extractFirstAndLastDigits', () => {
-	it('gets the first and last digits in the string', () => {
-		expect(extractFirstAndLastDigits('211a1')).toEqual(['2', '1']);
-		expect(extractFirstAndLastDigits('12')).toEqual(['1', '2']);
-		expect(extractFirstAndLastDigits('7eight7fmqpzrjlcctjvhrdcjgm2mgkqrbdcmzlzngbkqlj1')).toEqual([
-			'7',
-			'1',
-		]);
+	describe('without the readWords option', () => {
+		it('gets the first and last digits in the string', () => {
+			expect(extractFirstAndLastDigits('211a1')).toEqual([2, 1]);
+			expect(extractFirstAndLastDigits('12')).toEqual([1, 2]);
+			expect(extractFirstAndLastDigits('7eight7fmqpzrjlcctjvhrdcjgm2mgkqrbdcmzlzngbkqlj1')).toEqual(
+				[7, 1],
+			);
+		});
+
+		it('returns single digit as first and last digits when only one digit in string', () => {
+			expect(extractFirstAndLastDigits('1')).toEqual([1, 1]);
+			expect(extractFirstAndLastDigits('a2')).toEqual([2, 2]);
+		});
+
+		it('throws error if no digit is in string', () => {
+			const str = 'hello';
+			expect(() => extractFirstAndLastDigits(str)).toThrow(Error);
+		});
 	});
 
-	it('returns single digit as first and last digits when only one digit in string', () => {
-		expect(extractFirstAndLastDigits('1')).toEqual(['1', '1']);
-		expect(extractFirstAndLastDigits('a2')).toEqual(['2', '2']);
+	describe('without the readWords option', () => {
+		const options = { readWords: true };
+		it('gets the first and last digits in the string', () => {
+			expect(extractFirstAndLastDigits('13two', options)).toEqual([1, 2]);
+			expect(extractFirstAndLastDigits('eight71', options)).toEqual([8, 1]);
+		});
+
+		it('returns single digit as first and last digits when only one digit in string', () => {
+			expect(extractFirstAndLastDigits('two', options)).toEqual([2, 2]);
+			expect(extractFirstAndLastDigits('oonee', options)).toEqual([1, 1]);
+		});
+
+		it('throws error if no digit is in string', () => {
+			const str = 'hello';
+			expect(() => extractFirstAndLastDigits(str, options)).toThrow(Error);
+		});
+	});
+});
+
+describe('convertWordToNum', () => {
+	it('takes digit word and returns number', () => {
+		expect(convertWordToNum('five')).toBe(5);
 	});
 
-	it('throws error if no digit is in string', () => {
-		const str = 'hello';
-		expect(() => extractFirstAndLastDigits(str)).toThrow(Error);
+	it('throws error if word is not a digit word', () => {
+		expect(() => convertWordToNum('hello')).toThrow(Error);
 	});
 });
--- src/day-1.ts
+++ src/day-1.ts
@@ -3,34 +3,94 @@
 
 import { BunFile } from 'bun';
 
-export const fileTotaller = async (file: BunFile): Promise<number> => {
+interface Options {
+	// Are number words (one, two, three) read as digits?
+	readWords?: boolean;
+}
+
+export const fileTotaller = async (file: BunFile, options?: Options): Promise<number> => {
 	const text = await file.text();
 	const lines = text.split('\n');
-	const numbers = lines.map<number>((line) => combineDigits(extractFirstAndLastDigits(line)));
+	const numbers = lines.map<number>((line) =>
+		combineDigits(extractFirstAndLastDigits(line, options)),
+	);
 
 	return numbers.reduce((acc, cur) => {
 		return acc + cur;
 	}, 0);
 };
 
-export const combineDigits = (digits: [string, string]): number => {
+export const combineDigits = (digits: [number, number]): number => {
 	return +digits.join('');
 };
 
-export const extractFirstAndLastDigits = (str: string, readWords = false): [string, string] => {
-	// Regex to capture all digits in string
-	let digits: string[];
-	if (readWords) {
-		digits = ['1', '1'];
-	} else {
-		const rgx: RegExp = /[0-9]/g;
-		digits = [...str.matchAll(rgx)].flat();
-	}
-	if (digits === undefined || digits.length === 0) {
+export const extractFirstAndLastDigits = (str: string, options?: Options): [number, number] => {
+	// The trick here is that sometimes a string has two words that share a letter.
+	// ie. 'mcnine4sixrloneightspv' which has 'oneight'. The last number word in the
+	// string is eight, but that isn't picked up if you parse the string one letter
+	// at a time left to right.
+
+	// I'm going to rework this to take the string and find the first digit, then stop
+	// and reverse the string to find the first backwards digit and use that as the last.
+	// This worked, but ended up with a high amount of repetition. I'm moving on.
+
+	const readWords = options?.readWords;
+
+	const rgx = readWords ? /[0-9]|zero|one|two|three|four|five|six|seven|eight|nine/g : /[0-9]/g;
+	const rgxBackwards = readWords
+		? /[0-9]|orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin/g
+		: /[0-9]/g;
+
+	const digits = str.match(rgx);
+	if (digits === null) {
 		throw new Error(`No digits in str. Value of str is ${str}`);
 	}
-	const firstDigit = digits[0];
-	const lastDigit = digits.length === 1 ? digits[0] : digits[digits.length - 1];
+	const firstDigit = digits[0].length === 1 ? +digits[0] : convertWordToNum(digits[0]);
+
+	const strBackwards = str.split('').reverse().join('');
+	const digitsBackwards = strBackwards.match(rgxBackwards);
+	if (digitsBackwards === null) {
+		throw new Error(`No digits in str. Value of str is ${strBackwards}`);
+	}
+	const lastDigit =
+		digitsBackwards[0].length === 1 ? +digitsBackwards[0] : convertWordToNum(digitsBackwards[0]);
 
 	return [firstDigit, lastDigit];
+};
+
+export const convertWordToNum = (word: string): number => {
+	switch (word) {
+		case 'zero':
+		case 'orez':
+			return 0;
+		case 'one':
+		case 'eno':
+			return 1;
+		case 'two':
+		case 'owt':
+			return 2;
+		case 'three':
+		case 'eerht':
+			return 3;
+		case 'four':
+		case 'ruof':
+			return 4;
+		case 'five':
+		case 'evif':
+			return 5;
+		case 'six':
+		case 'xis':
+			return 6;
+		case 'seven':
+		case 'neves':
+			return 7;
+		case 'eight':
+		case 'thgie':
+			return 8;
+		case 'nine':
+		case 'enin':
+			return 9;
+		default:
+			throw new Error(`Word ${word} is not a number word`);
+	}
 };
